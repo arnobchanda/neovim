ba50368a06e961c8a316296b7b5b935c0536c9c2
Arnob Chanda <arnob.chanda@aesi-ess.com>
2025-03-27 15:57:32 +0530

    Updating neotree for real-time updates

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -7,7 +7,9 @@ return {
     "MunifTanjim/nui.nvim",
   },
   opts = {
+    enable_git_status = true,
     filesystem = {
+      use_libuv_file_watcher = true,
       filtered_items = {
         hide_dotfiles = false,
         visible = true,
